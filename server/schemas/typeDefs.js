956cebd931b6d105733786f1c8f3e6b7eb9eb2ab
Reeggie Mace <reginaldmace36@gmail.com>
2021-07-31 12:50:58 -0400

    added schema folder to server folder with resolver and typeDef files

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { ggl } = require("apollo-server-express");
+
+const typeDefs = ggl`
+type City {
+    _id: ID!
+    name: String!
+}
+type Park {
+    _id: ID!
+    name: String!
+    park_size: String!
+    review: String!
+    poop_bags: Boolean!
+    has_fence: Boolean!
+    leash_required: Boolean!
+    is_supervised: Boolean!
+    cityId: ID!
+    userID: ID!
+
+}
+type User {
+    _id: ID!
+    user_name: String!
+    password: String!
+}
+type Query {
+    city: [City]
+    parks(_id: String!): [Park]
+}
+type Mutation {
+    
+}`;
+
+module.exports = typeDefs;
