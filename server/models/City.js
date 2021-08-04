0858075a69fe21b8b92d94bf5a6e65dd05bf293c
Reeggie Mace <reginaldmace36@gmail.com>
2021-08-04 19:14:04 -0400

    added server updates

--- server/models/City.js
+++ server/models/City.js
@@ -1,13 +1,15 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const Park = require("./Park");
 
 const citySchema = new Schema({
   name: {
     type: String,
     required: true,
     unique: true,
-  }
+  },
+  parks: [{ type: Schema.Types.ObjectId, ref: Park }],
 });
 
-const City = model('City', citySchema);
+const City = model("City", citySchema);
 
 module.exports = City;
--- server/models/User.js
+++ server/models/User.js
@@ -1,18 +1,18 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
   username: {
     type: String,
-    required: true,
+    // required: "please enter username",
     unique: true,
   },
   password: {
     type: String,
     required: true,
     unique: true,
-  }
+  },
 });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
